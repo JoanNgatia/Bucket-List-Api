99c65b6629a74b28d07edc1b7f1ead2e73702c17
andela-jngatia <joan.ngatia@andela.com>
2016-02-03 00:00:34 +0300

    (Feature) Add userlogin endpoint

--- app/manage.py
+++ app/manage.py
@@ -2,12 +2,13 @@
 
 from flask import Flask
 from flask.ext.restful import Api
-from resources import UserRegistration, BucketListAll, BucketListId, BucketListItemAdd, BucketListItemEdit
+from resources import UserRegistration, UserLogin, BucketListAll, BucketListId, BucketListItemAdd, BucketListItemEdit
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(UserRegistration, '/auth/register')
+api.add_resource(UserRegistration, '/auth/register/')
+api.add_resource(UserLogin, '/auth/login/')
 api.add_resource(BucketListAll, '/bucketlists/')
 api.add_resource(BucketListId, '/bucketlists/<list_id>/')
 api.add_resource(BucketListItemAdd, '/bucketlist/<list_id>/item/')
