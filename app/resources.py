ecf74c73ce719cf61d6010619b9efb9b9e3ff26e
andela-jngatia <joan.ngatia@andela.com>
2016-02-08 20:40:22 +0300

    (Fix) Refactor routes to allow for token based authentication

--- app/resources.py
+++ app/resources.py
@@ -1,16 +1,20 @@
-# from sqlalchemy import DateTime
+from sqlalchemy import DateTime
 from models import User, BucketList, BucketListItems
 from db import session
 
-from flask_httpauth import HTTPBasicAuth
-from flask.ext.restful import reqparse, abort, Resource, fields, marshal_with
-from flask.ext.login import login_required
+from flask import g, request
+# from flask_httpauth import HTTPBasicAuth
+from flask.ext.restful import reqparse, abort, Resource, fields, marshal_with, marshal
+from flask.ext.login import LoginManager, login_required, current_user
+# from manage import login_manager
+# from sqlalchemy_paginator import Paginator
 
-auth = HTTPBasicAuth()
+# auth = HTTPBasicAuth()
+
 users = {
     'user_id': fields.Integer,
     'username': fields.String,
-    'password': fields.String
+    'password_hash': fields.String
 }
 
 bucketlistitems = {
@@ -27,13 +31,15 @@ bucketlists = {
     'id': fields.Integer(attribute='list_id'),
     'list_name': fields.String,
     'items': fields.Nested(bucketlistitems),
-    'creator': fields.String,
-    # 'date_created': fields.DateTime,
-    # 'date_modified': fields.DateTime
+    # 'creator': fields.String,
+    'date_created': fields.DateTime,
+    'date_modified': fields.DateTime
 }
 
 
 parser = reqparse.RequestParser()
+
+
 
 
 class UserRegistration(Resource):
@@ -50,12 +56,12 @@ class UserRegistration(Resource):
         exists = session.query(User).filter_by(username=username).first()
         if exists:
             return {'message': 'User already exists!'}
-        else:
-            user = User(username=username, password=password)
-            session.add(user)
-            session.commit()
-            return {'message': 'User {} has been successfully registered'
-                               .format(username)}
+        user = User(username=username)
+        user.hash_password(password)
+        session.add(user)
+        session.commit()
+        return {'message': 'User {} has been successfully registered'
+                           .format(username)}
 
 
 class UserLogin(Resource):
@@ -65,64 +71,74 @@ class UserLogin(Resource):
         """Log in a user."""
         parser.add_argument('username')
         parser.add_argument('password')
-
         args = parser.parse_args()
         username = args['username']
-        password = args['password']
+        password_hash = args['password']
+
         userlogged = session.query(User).filter_by(username=username).first()
-        # user = User.verify_password(username, password)
-        # if user:
-        if User.verify_password(userlogged, password):
-            token = User.generate_confirmation_token(userlogged, username)
-            return token
-        return False
+        if userlogged:
+            if userlogged.verify_password(password_hash):
+                token = userlogged.generate_confirmation_token()
+                userlogged.confirmed = True
+                session.add(userlogged)
+                session.commit()
+                return {'token': token}
+            # if password not verified
+            return {'message': 'Password not verified'}
+        # if user doesn't exist
+        return {'message': "User doesn't exist"}
 
 
 class BucketListAll(Resource):
-    """Resource to handle '/bucketlists/'endpoint."""
 
-    @auth.login_required
-    @marshal_with(bucketlists)
+    """
+    Resource to handle '/bucketlists/' and
+    '/bucketlists/<int:page>' endpoint.
+    """
+
+    @login_required
     def get(self):
         """Retrieve all bucketlists belonging to the logged in user."""
-        bucketlist = session.query(BucketList).all()
-        return bucketlist
+        # parser.add_argument('page')
+        # args = parser.parse_args()
+        # pageshow = args[page]
+        # limit = 20
+        created_by = current_user.user_id
+        bucketlist = session.query(BucketList).filter_by(creator=created_by).all()
+        return marshal(bucketlist, bucketlists)
 
-    @auth.login_required
-    @marshal_with(bucketlists)
+    @login_required
     def post(self):
         """Create a new bucketlist."""
         parser.add_argument('list_name')
         args = parser.parse_args()
         list_name = args['list_name']
-
         bucketlistexist = session.query(BucketList).filter_by(
             list_name=list_name).first()
+        created_by = current_user.user_id
         if bucketlistexist:
             return {'message': 'Bucketlist  already exists'}
         else:
-            bucketlist = BucketList(list_name=list_name)
+            bucketlist = BucketList(list_name=list_name, creator=created_by)
             session.add(bucketlist)
             session.commit()
-            return bucketlists, 201
+            return marshal(bucketlist, bucketlists), 201
 
 
 class BucketListId(Resource):
     """Resource to handle '/bucketlist/<list_id>' endpoint."""
 
-    @auth.login_required
-    @marshal_with(bucketlists)
+    @login_required
     def get(self, list_id):
         """Retrieve a particular bucketlist belonging to logged in user."""
         bucketlist = session.query(BucketList).filter_by(
             list_id=list_id).first()
         if bucketlist:
-            return bucketlist
+            return marshal(bucketlist, bucketlists)
 
-        abort(404, message="Bucketlist {} does not exist".format(id))
+        return {'message': 'Bucketlist {} does not exist'.format(list_id)}
 
-    @auth.login_required
-    @marshal_with(bucketlists)
+    @login_required
     def put(self, list_id):
         """Modify existing bucketlist."""
         bucketlistedit = session.query(
@@ -138,8 +154,7 @@ class BucketListId(Resource):
         return {'message': 'Bucketlist {} has not been found'
                 .format(list_id)}
 
-    @auth.login_required
-    @marshal_with(bucketlists)
+    @login_required
     def delete(self, list_id):
         """Delete an existing bucketlist."""
         bucketlistdelete = session.query(
@@ -152,8 +167,7 @@ class BucketListId(Resource):
 class BucketListItemAdd(Resource):
     """Resource to handle '/bucketlist/<list_id>/item'."""
 
-    @auth.login_required
-    @marshal_with(bucketlistitems)
+    @login_required
     def post(self, list_id):
         """Add a new item to a bucketlist."""
         bucketlistfind = session.query(
@@ -166,17 +180,15 @@ class BucketListItemAdd(Resource):
                 item_name=item, bucketlist=bucketlistfind)
             session.add(bucketlistitem)
             session.commit()
-            # return {'message': 'An item has been added to Bucketlist {}'
-                                # .format(list_id)}
-            return bucketlistitem
+            return {'message': '{} has been added to Bucketlist {}'
+                               .format(item, list_id)}
         return {'message': 'Bucketlist does not exist'}
 
 
 class BucketListItemEdit(Resource):
     """Resource to handle '/bucketlist/<list_id>/item/<item_id/'."""
 
-    @auth.login_required
-    @marshal_with(bucketlistitems)
+    @login_required
     def put(self, list_id, item_id):
         """Update an existing bucketlist item."""
         bucketlistfind = session.query(
@@ -195,8 +207,7 @@ class BucketListItemEdit(Resource):
         return {'message': 'Bucketlist {} has not been found'
                 .format(item_id)}
 
-    @auth.login_required
-    @marshal_with(bucketlistitems)
+    @login_required
     def delete(self, list_id, item_id):
         """Delete an item form an existing bucketlist."""
         bucketlistfind = session.query(
