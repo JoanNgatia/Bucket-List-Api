ba772d69b3416a2531e63be53b875e916c67ae45
andela-jngatia <joan.ngatia@andela.com>
2016-02-09 15:33:47 +0300

    (Chore) Refactor tests on User model

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -2,19 +2,17 @@ import unittest
 import nose
 
 from app.models import User
-# from models import User
 
 
 class UserModelTestCase(unittest.TestCase):
     def test_password_setter(self):
-        u = User(password='cat')
+        u = User(password_hash='cat')
         self.assertTrue(u.password_hash is not None)
 
     def test_password_verification(self):
-        u = User(password='cat')
+        u = User(password_hash='cat')
         self.assertTrue(u.verify_password('cat'))
         self.assertFalse(u.verify_password('dog'))
 
 if __name__ == '__main__':
-    # unittest.main()
     nose.run()
