dcddd91ce1e18b4b465e8f57a4dd978fa4cb6bf8
andela-jngatia <joan.ngatia@andela.com>
2016-02-09 23:18:49 +0300

    (Chore) Refactor and separate authentictaion code

---
+++ app/authentication.py
@@ -0,0 +1,55 @@
+"""
+     This files handles the authentication logic.
+"""
+from flask.ext.restful import reqparse, Resource
+
+from models import User
+from db import session
+
+parser = reqparse.RequestParser()
+
+
+class UserRegistration(Resource):
+    """Resource to handle '/auth/register/' endpoint."""
+
+    def post(self):
+        """Allow a user to register."""
+        parser.add_argument('username')
+        parser.add_argument('password')
+
+        args = parser.parse_args()
+        username = args['username']
+        password = args['password']
+        exists = session.query(User).filter_by(username=username).first()
+        if exists:
+            return {'message': 'User already exists!'}
+        user = User(username=username)
+        user.hash_password(password)
+        session.add(user)
+        session.commit()
+        return {'message': 'User {} has been successfully registered'
+                           .format(username)}
+
+
+class UserLogin(Resource):
+    """Resource to handle '/auth/login/' endpoint."""
+
+    def post(self):
+        """Log in a user."""
+        # parser.add_argument('username', type="str", required=True)
+        parser.add_argument('username')
+        parser.add_argument('password')
+        args = parser.parse_args()
+        username = args['username']
+        password_hash = args['password']
+
+        userlogged = session.query(User).filter_by(username=username).first()
+
+        if not userlogged:
+            return {'message': "User doesn't exist"}
+        if userlogged.verify_password(password_hash):
+            token = userlogged.generate_confirmation_token()
+            return {'token': token}
+        # if password not verified
+        return {'message': 'Password not verified'}
+        # if user doesn't exist
