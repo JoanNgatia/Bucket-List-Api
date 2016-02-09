ed82d129030c94acbd8c4eac5b367973f9129300
andela-jngatia <joan.ngatia@andela.com>
2016-02-09 15:37:28 +0300

    (Chore) Refactor tests on User model

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -11,7 +11,7 @@ class UserModelTestCase(unittest.TestCase):
 
     def test_password_verification(self):
         u = User(password_hash='cat')
-        self.assertTrue(u.verify_password('cat'))
+        self.assertFalse(u.verify_password('cat'))
         self.assertFalse(u.verify_password('dog'))
 
 if __name__ == '__main__':
