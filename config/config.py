d27160800071b5af97e11809c1f8937bdbb47a2e
andela-jngatia <joan.ngatia@andela.com>
2016-02-17 17:24:09 +0300

    (Fix) Set configurations for different environments

--- config/config.py
+++ config/config.py
@@ -5,24 +5,27 @@ class Config(object):
     """Default configurations."""
 
     DEBUG = True
-    TESTING = False
-    # CSRF_ENABLED = True
+    TESTING = True
     SECRET_KEY = os.environ['SECRET_KEY']
+    DATABASE_URL = 'sqlite:///app/bucketlist.db'
 
 
 class TestingConfig(Config):
     """Testing configurations."""
 
-    directory = os.path.dirname(os.path.abspath(__file__))
-    test_db = os.path.join(directory, '../tests/test.db')
-
     TESTING = True
-    DATABASE_URL = 'sqlite:///' + test_db
+    DATABASE_URL = 'sqlite:///tests/test.db'
 
 
 class DevelopmentConfig(Config):
-    """
-    Development configurations.
-    """
+    """Development configurations."""
+
     DEVELOPMENT = True
     DEBUG = True
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'default': DevelopmentConfig
+}
