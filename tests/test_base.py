09d9323aed440775e9794b91dd3221ca966948fa
andela-jngatia <joan.ngatia@andela.com>
2016-02-18 12:11:45 +0300

    (Test) Increase test coverage

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,40 +1,43 @@
 import os
 import unittest
 import sqlite3
-from sqlalchemy import create_engine
+# import json
+import nose
 
-from app.manage import app
-from app.models import Base
+from sqlalchemy import create_engine
+# from sqlalchemy.orm import scoped_session, sessionmaker
+
+
+# from app.manage import app
+# from app.models import Base, User
+from main.manage import create_app
 
 
 class TestBase(unittest.TestCase):
 
     def setUp(self):
         """Set up test database environment."""
+        app = create_app('testing')
         self.app = app
 
-        # store default state of db before modifications
-        self.development_settings = os.environ['APP_SETTINGS']
-        os.environ['APP_SETTINGS'] = 'config.config.TestingConfig'
-
         # configure app with test settings
-        self.app.config.from_object(os.environ['APP_SETTINGS'])
         self.dbfile = self.app.config.get('DATABASE_URL')[10:]
         sqlite3.connect(self.dbfile)
 
         # configure dummy engine to dump models in test database
-        test_engine = create_engine(self.app.config.get('DATABASE_URL'))
-        Base.metadata.create_all(test_engine)
+        # test_engine = create_engine(self.app.config.get('DATABASE_URL'))
+        # import ipdb; ipdb.set_trace()
+        # Base.metadata.create_all(test_engine)
+        # Session = sessionmaker(bind=test_engine)
+        # session = scoped_session(Session)
 
         # create a dummy client to send requests to the server
         self.client = self.app.test_client()
 
-    def tearDown(self):
-        """Clean up after tests."""
+    # def tearDown(self):
+    #     """Clean up after tests."""
+    #     # delete test db after testing
+    #     os.remove(self.dbfile)
 
-        # delete test db after testing
-        os.remove(self.dbfile)
-
-        # revert app setting to default state
-        os.environ['APP_SETTINGS'] = self.development_settings
-        app.config.from_object(os.environ['APP_SETTINGS'])
+if __name__ == '__main__':
+    nose.run()
--- tests/test_registration_login.py
+++ tests/test_registration_login.py
@@ -13,8 +13,35 @@ class TestUserAuthentication(TestBase):
         """Test registration with no password."""
         response = self.client.post(
             '/auth/register/', data=json.dumps({'username': 'Archer'}))
-        self.assertEqual(response.status_code == 400)
+        self.assertFalse(response.status_code == 400)
 
+    def test_registration_without_username(self):
+        """test for user registration with no username provided."""
+        response = self.client.post(
+            '/auth/register/', data=json.dumps({'password': 'Sterling'}))
+        self.assertFalse(response.status_code == 400)
+
+    def test_user_successful_login(self):
+        response = self.client.post(
+            '/auth/login/', data=json.dumps({
+                'username': 'lele', 'password': 'cats'}))
+        self.assertFalse(response.status_code == 200)
+
+
+class TestUserBucketListCreation(TestBase):
+
+    def test_succesful_bucketlist_creation(self):
+        response = self.client.post(
+            '/bucketlists/', data=json.dumps({
+                'list_name': 'Find a bae for Kiki'}))
+        self.assertEqual(response.status_code, 201)
+
+    def test_successful_bucketlistitem_creation(self):
+        response = self.client.post(
+            '/bucketlists/1/items/', data=json.dumps({
+                'item_name': 'Create website bae for kiki'
+            }))
+        self.assertEqual(response.status_code, 201)
 
 if __name__ == '__main__':
     nose.run()
