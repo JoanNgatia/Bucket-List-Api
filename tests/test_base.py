9963e82a87793765bb482e2864c50c684cf082ec
andela-jngatia <joan.ngatia@andela.com>
2016-02-19 14:56:40 +0300

    (Chore) Create Test base class

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,57 +1,32 @@
-import os
-import unittest
-import sqlite3
-import json
 import nose
 
-from flask import g, url_for
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-from faker import Factory
-# from app.manage import app
-from app.models import Base, User
-from main.manage import create_app, db
+from flask.ext.testing import TestCase
+from config.config import TestingConfig
 
-fake = Factory.create()
+from main.manage import app, db
+from app.database import session, init_db, Base
 
-class TestBase(unittest.TestCase):
-    username = fake.user_name()
-    password = fake.password()
+
+class BaseTestCase(TestCase):
+    """A base test case for flask-tracking."""
+
+    def create_app(self):
+        """method for setting up config variables for the app"""
+        app.config.from_object(TestingConfig)
+        return app
 
     def setUp(self):
-        # import ipdb
-        # ipdb.set_trace()
+        """setUp method"""
+        self.app = app.test_client()
+        # db.create_all()
+        init_db()
 
-        """Set up test database environment."""
-        app = create_app('testing')
-        self.app = app
-
-        # # self.dbfile = self.app.config.get('DATABASE_URL')[10:]
-        # self.dbfile = self.app.config.get('DATABASE_URL')
-        # sqlite3.connect(self.dbfile)
-        # set current flask context for request handling during testing
-        self.app_context = self.app.app_context()
-        self.app_context.push()
-        with self.app_context:
-            db.init_app(self.app)
-            db.create_all()
-        # create a dummy client to send requests to the server
-        self.client = app.test_client()
-
-    def test_registration_without_password(self):
-        """Test registration with no password."""
-        # import ipdb; ipdb.set_trace()
-        response = self.client.post('/register',
-                                    data=json.dumps({'username': self.username}), content_type='application/json')
-
-        # import ipdb; ipdb.set_trace()
-        self.assertTrue(response.status_code == 400)
-    # def tearDown(self):
-    #     """Clean up after tests."""
-    # # delete test db after testing
-    #     os.remove(self.dbfile)
-    #     # db.session.remove()
-        # db.drop_all()
+    def tearDown(self):
+        """method for clearing all settings"""
+        session.remove()
+        # Base.drop_all()
+        # Base.remove()
+        # Base.drop_all()
 
 if __name__ == '__main__':
     nose.run()
