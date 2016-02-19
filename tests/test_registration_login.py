425002899b66d4752b7791c6f6645534c8e8cf43
andela-jngatia <joan.ngatia@andela.com>
2016-02-19 14:57:14 +0300

    (Chore) Test for user authentication

--- tests/test_registration_login.py
+++ tests/test_registration_login.py
@@ -1,52 +1,61 @@
-import nose
-import json
 
-from test_base import TestBase
 from flask import url_for
+import json
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from test_base import BaseTestCase
 from app.models import User
+from app.database import session
+
+from faker import Factory
+
+fake = Factory.create()
 
 
-class TestUserAuthentication(TestBase):
-    # defaultusername = 'Archer'
-    # defaultpassword = 'Sterling'
-    # def create_dummy_user(self):
-    #     self.defaultusername = 'Archer'
-    #     self.defaultpassword = 'Sterling'
-    #     self.dummy_user = User(username=self.defaultusername, password_hash=self.defaultpassword)
+class SignUpViewTests(BaseTestCase):
+    """This class contains all tests for user creation and signin."""
 
-    def test_registration_without_password(self):
-        """Test registration with no password."""
-        response = self.client.post('/auth/register',
-                                    data=json.dumps({'username': 'Archer'}))
-        self.assertTrue(response.status_code == 400)
+    def setUp(self):
+        """Instantiate dummy test details."""
+        self.username = fake.user_name()
+        self.password = fake.password()
+        password_hash = generate_password_hash(self.password)
+        self.user = User(username=self.username, password_hash=password_hash)
+        session.add(self.user)
+        session.commit()
 
-    def test_registration_without_username(self):
-        """test for user registration with no username provided."""
-        response = self.client.post(
-            '/auth/register/', data=json.dumps({'password': 'Sterling'}))
-        self.assertTrue(response.status_code == 400)
-
-    def test_user_successful_login(self):
-        response = self.client.post(
-            '/auth/login/', data=json.dumps({
-                'username': 'lele', 'password': 'cats'}))
-        self.assertTrue(response.status_code == 200)
-
-
-class TestUserBucketListCreation(TestBase):
-
-    def test_succesful_bucketlist_creation(self):
-        response = self.client.post(
-            '/bucketlists/', data=json.dumps({
-                'list_name': 'Find a bae for Kiki'}))
+    def test_user_registration(self):
+        """Test that creation of a user succeeds when correct info is sent."""
+        username = fake.user_name()
+        password = fake.password()
+        response = self.client.post(url_for('register'),
+                                    data=json.dumps({'username': username,
+                                                     'password': password}),
+                                    content_type='application/json')
+        self.assertIn('User {} has been successfully registered'.
+                      format(username), response.data)
         self.assertEqual(response.status_code, 201)
 
-    def test_successful_bucketlistitem_creation(self):
-        response = self.client.post(
-            '/bucketlists/1/items/', data=json.dumps({
-                'item_name': 'Create website bae for kiki'
-            }))
-        self.assertEqual(response.status_code, 201)
+    def test_registration_no_password(self):
+        """Test that a user is not allowed to register without a password."""
+        response = self.client.post(url_for('register'),
+                                    data=json.dumps({'username': self.username}),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('Missing fields!', response.data)
 
-if __name__ == '__main__':
-    nose.run()
+    def test_registration_no_username(self):
+        """Test that a user cannot register without a username."""
+        response = self.client.post(url_for('register'),
+                                    data=json.dumps({'password': self.password}),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('Missing fields!', response.data)
+
+    def test_user_login(self):
+        """Test that a user can successfuly login."""
+        response = self.client.post(url_for('login'),
+                                    data=json.dumps({'username': self.username,
+                                                     'password': self.password}),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 201)
