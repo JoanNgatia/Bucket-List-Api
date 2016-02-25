bfb53b99adefe4391c2e670829e44391c155c36c
andela-jngatia <joan.ngatia@andela.com>
2016-02-25 14:29:19 +0300

    (Chore) Increase test coverage

--- tests/test_bucketlist_endpoint.py
+++ tests/test_bucketlist_endpoint.py
@@ -39,24 +39,33 @@ class TestBucketLists(BaseTestCase):
                                          content_type='application/json')
         self.token = json.loads(self.response.data)['token']
 
-    # def tearDown(self):
-    #     """Delete details created at end of test."""
-    #     del self.username
-    #     del self.password
-    #     del self.user
-    #     del self.bucketlist
-    #     del self.token
-    #     session.rollback()
-    #     session.remove()
-
-    def test_unauthorized_bucketlist_methods(self):
-        """Test unsuccessful bucketlist retrieval and  /
-        creation for unauthorized users."""
+    def test_unauthorized_methods(self):
+        """Test unsuccessful bucketlist retrieval and
+        creation for unauthorized users.
+        """
+        # Test that a user has to have a token to access bucketlists
         response = self.client.get(url_for('bucketlists'))
         self.assertEqual(response.status_code, 401)
 
         response = self.client.post(url_for('bucketlists'))
         self.assertEqual(response.status_code, 401)
+
+        # Test that a user can only access bucketlists that they create.
+        bucketlist = session.query(BucketList).first()
+        url2 = '/bucketlists/{}/'.format(bucketlist.list_id)
+        response = self.client.get(url2,
+                                   headers={'token': self.token})
+        self.assertIn('Bucketlist {} not found'.format(bucketlist.list_id),
+                      response.data)
+
+        response2 = self.client.put(url2, data=json.dumps(
+            {'list_name': fake.name()}), headers={'token': self.token})
+        self.assertIn('Bucketlist {} has not been found'
+                      .format(bucketlist.list_id), response2.data)
+
+        response3 = self.client.delete(url2, headers={'token': self.token})
+        self.assertIn('Bucketlist {} has not been found'
+                      .format(bucketlist.list_id), response3.data)
 
     def test_bucketlist_creation(self):
         """Test successful bucketlist creation."""
@@ -69,6 +78,14 @@ class TestBucketLists(BaseTestCase):
                                     headers={'token': self.token})
         self.assertEqual(response.status, '201 CREATED')
         self.assertIn(bucketlist1['list_name'], response.data)
+
+        # Test that a user cannot pass in an invalid type
+        bucketlist2 = {
+            'item_type': fake.name()
+        }
+        response = self.client.post(url_for('bucketlists'), data=bucketlist2,
+                                    headers={'token': self.token})
+        self.assertIn('Invalid value passed.', response.data)
 
         # test that a user cannot create two bucketlists with similar names
         response = self.client.post(url_for('bucketlists'),
@@ -86,11 +103,11 @@ class TestBucketLists(BaseTestCase):
     def test_bucketlist_manipulation(self):
         """Test that a user can get or delete a specific bucketlist."""
         url = '/bucketlists/{}/'.format(self.bucketlist.list_id)
-
         # Test user can get bucketlist by ID
         response = self.client.get(url,
                                    headers={'token': self.token})
         self.assertEqual(response.status_code, 200)
+        self.assertIn(self.list_name, response.data)
 
         # Test a user can update a particular bucketlist
         name2 = {
@@ -99,6 +116,18 @@ class TestBucketLists(BaseTestCase):
         response = self.client.put(url, data=name2,
                                    headers={'token': self.token})
         self.assertEqual(response.status_code, 202)
+
+        # check that updated bucketlist has the new value
+        response2 = self.client.get(url,
+                                    headers={'token': self.token})
+        self.assertIn(name2['list_name'], response2.data)
+
+        name3 = {
+            'list_type': fake.name()
+        }
+        response = self.client.put(url, data=name3,
+                                   headers={'token': self.token})
+        self.assertIn('Invalid value passed.', response.data)
 
         # Test a user can delete a particular bucketlist
         response = self.client.delete(url,
@@ -123,5 +152,10 @@ class TestBucketLists(BaseTestCase):
         response = self.client.get('/bucketlists/?limit=5',
                                    headers={'token': self.token})
         item_dict = json.loads(response.data)
-        self.assertLessEqual(len(item_dict), 5)
+        self.assertLessEqual(len(item_dict), 20)
         self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/bucketlists/?limit=200',
+                                   headers={'token': self.token})
+        item_dict = json.loads(response.data)
+        self.assertLessEqual(len(item_dict), 100)
